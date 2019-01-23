d2e4f919040ecdc495e029672b582bb3475e64e8
Andrew Mello <driwundernight@gmail.com>
2019-01-23 00:53:08 -0200

    0.3.5.212 - Adicionado CustomerProfileBoundary referente a rev211.

---
+++ src/tercom/boundary/dashboard/CustomerProfileBoundary.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace tercom\boundary\dashboard;
+
+use dProject\restful\template\ApiTemplateResult;
+use dProject\restful\ApiContent;
+use tercom\core\System;
+
+/**
+ * @see BoundaryManager
+ * @author Andrew
+ */
+class CustomerProfileBoundary extends DefaultDashboardLoggedBoundary
+{
+	/**
+	 * @var string
+	 */
+	public const BASE_PATH = 'CustomerProfile/';
+
+	/**
+	 * {@inheritDoc}
+	 * @see \dProject\restful\ApiServiceInterface::init()
+	 */
+	public function init()
+	{
+		parent::init();
+
+		$this->setNavSideActive('customerProfile');
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \dproject\restful\template\ApiTemplate::callIndex()
+	 */
+	public function callIndex()
+	{
+		return $this->onList(System::getApiConnection()->getContent());
+	}
+
+	/**
+	 * @ApiAnnotation({"params"["idCustomer"]})
+	 * @param ApiContent $content
+	 * @return ApiTemplateResult
+	 */
+	public function onList(ApiContent $content): ApiTemplateResult
+	{
+		$dashboardTemplate = $this->newBaseTemplate();
+		$dashboardTemplate = $this->prepareInclude(self::BASE_PATH. 'CustomerProfileList');
+		$dashboardTemplate->idCustomer = $this->getCustomerId($content);
+
+		$result = new ApiTemplateResult();
+		$result->add($dashboardTemplate);
+
+		return $result;
+	}
+
+	/**
+	 * @ApiAnnotation({"params"["idCustomer"]})
+	 * @param ApiContent $content
+	 * @return ApiTemplateResult
+	 */
+	public function onAdd(ApiContent $content): ApiTemplateResult
+	{
+		$dashboardTemplate = $this->newBaseTemplate();
+		$dashboardTemplate = $this->prepareInclude(self::BASE_PATH. 'CustomerProfileAdd');
+		$dashboardTemplate->idCustomer = $this->getCustomerId($content);
+
+		$result = new ApiTemplateResult();
+		$result->add($dashboardTemplate);
+
+		return $result;
+	}
+
+	/**
+	 * @ApiAnnotation({"params":["idCustomerProfile","idCustomer"]})
+	 * @param ApiContent $content
+	 * @return ApiTemplateResult
+	 */
+	public function onView(ApiContent $content): ApiTemplateResult
+	{
+		$dashboardTemplate = $this->newBaseTemplate();
+		$dashboardTemplate = $this->prepareInclude(self::BASE_PATH. 'CustomerProfileView');
+		$dashboardTemplate->idCustomer = $this->getCustomerId($content);
+		$dashboardTemplate->idCustomerProfile = $content->getParameters()->getInt('idCustomerProfile');
+
+		$result = new ApiTemplateResult();
+		$result->add($dashboardTemplate);
+
+		return $result;
+	}
+
+	/**
+	 * @ApiAnnotation({"params":["idCustomerProfile","idCustomer"]})
+	 * @param ApiContent $content
+	 * @return ApiTemplateResult
+	 */
+	public function onRemove(ApiContent $content): ApiTemplateResult
+	{
+		$dashboardTemplate = $this->newBaseTemplate();
+		$dashboardTemplate = $this->prepareInclude(self::BASE_PATH. 'CustomerProfileRemove');
+		$dashboardTemplate->idCustomer = $this->getCustomerId($content);
+		$dashboardTemplate->idCustomerProfile = $content->getParameters()->getInt('idCustomerProfile');
+
+		$result = new ApiTemplateResult();
+		$result->add($dashboardTemplate);
+
+		return $result;
+	}
+}
+
